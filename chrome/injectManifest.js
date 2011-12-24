a5bdc50630e0dbc13c6186f03f9ca95bd0603eaf
Raine <raineorshine@gmail.com>
2011-12-24 12:09:31 -0700

    A little more re-organizing.

---
+++ chrome/injectManifest.js
@@ -0,0 +1,4 @@
+/** Uses injectScript to inject the source of scripts that need access to the host page's Javascript. This is a workaround for Chrome's sandboxing restriction. To embed our functionality into the Trello page, we need to interact with the page's JS. */
+injectScript(livequery_source);
+injectScript(trello_clone_source);
+injectScript(trello_clone_button_source);
