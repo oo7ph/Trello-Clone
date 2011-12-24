7556b9da9651110316c992b46fd5df50ff4a0529
Raine <raineorshine@gmail.com>
2011-12-24 16:59:49 -0700

    Change 'Copy' wording to 'Clone'.

--- chrome/source/trello_clone_card.js
+++ chrome/source/trello_clone_card.js
@@ -1,27 +1,27 @@
-// attach an additional click event to the card-menu element which will add a "Copy Card" link to the popover
+// attach an additional click event to the card-menu element which will add a "Clone Card" link to the popover
 $(".card-operation.js-card-menu").livequery("click", function() { 
 	var cardEl = $(this).parents(".list-card:first");
 
 	// set a short timeout to wait for the popover to appear
 	setTimeout(function() {
-		addCopyCardLink(cardEl.data("card"));
+		addClone(cardEl.data("card"));
 	}, 50);
 });
 
-/** Adds a "Copy Card" link to the currently active popover. */
-var addCopyCardLink = function(cardModel) {
+/** Adds a "Clone Card" link to the currently active popover. */
+var addClone = function(cardModel) {
 
 	// create the link
-	var copyCardLink = $("<a>Copy Card</a>");
+	var cloneCardLink = $("<a>Clone Card</a>");
 
 	// bind a click event that copies the card to the same list
-	copyCardLink.bind("click", function() {
-		copyCard(cardModel.getBoard().id, cardModel.getList().id, cardModel);
+	cloneCardLink.bind("click", function() {
+		cloneCard(cardModel.getBoard().id, cardModel.getList().id, cardModel);
 		$(".pop-over").hide();
 	});
 
 	// add the link to the end of the pop over menu
 	$(".pop-over-list").append(
-		$("<li>").append(copyCardLink)
+		$("<li>").append(cloneCardLink)
 	);
 };
