6ad1bf96a30fc65f6178a241ca4bfd52bf0271a6
loranger <loranger@free.fr>
2012-05-21 14:15:06 +0200

    [Upg] Re-enable "Clone Board" feature

--- chrome/source/trello_clone_button.js
+++ chrome/source/trello_clone_button.js
@@ -8,15 +8,15 @@
 */
 
 /** Adds a button to the right board widget content area that allows the user to clone the current board. */
-$("a:contains(Board Menu)").livequery(function() {
-	var boardContent = $(this).parent();
-	var button = $('<a class="button-link highlight-icon"> Clone Board </a>');
-	button.click(function() {
+$("a:contains(Unpin from)").livequery(function() {
+	var boardContent = $(this).parent().parent();
+	var button = $('<li><a class="js-clone-board"> Clone Board </a></li>');
+	$('a', button).click(function() {
 		var name = prompt("Enter a name for the new board:", "Clone of " + boardView.model.get("name"));
 		if(name) {
 			cloneCurrentBoard(name);
 		}
 	});
 
-	boardContent.append(button);
+	button.insertAfter($(this));
 });
